4df4cea2e0a3c30b7be08d4dfaa2d92f4548cff0
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-04-15 12:30:21 +0530

    messages added

--- yot/__main__.py
+++ yot/__main__.py
@@ -3,16 +3,17 @@ import requests
 
 words = ['word1', 'word2', 'word3']
 def main():
+    print("☠ You Own It Controller ☠")
     args = sys.argv[1:]
-    print("args: ", args)
+    print()
     if len(args) == 2:
         args1 = args[0]
         args2 = args[1]
         if args1 in words:
             r = requests.put(f'http://localhost:5000/api/{args1}', data = {'message': args2})
-            print("Refresh the Website to view your Results!")
+            print("♻ Refresh the Website to view your Results!")
         else:
-            print("Please enter a Valid Word Identifier\nAvaiable words identifiers are: [word1, word2, word3]")
+            print("🟡 Please enter a Valid Word Identifier\nAvaiable words identifiers are: [word1, word2, word3]")
         
     else:
-        print("Please enter Args./nUsage: yot [identifier] [word]")
+        print("🛑Please enter Args. \n Usage: yot [identifier] [message]")
